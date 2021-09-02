c2ff4b2167e59eaf369d2cbb267f2663055b0401
Piyush Gandhi <gpiyush211.pg@gmail.com>
2021-09-02 21:19:49 +0530

    first-commit

---
+++ public/js/pages/nav.js
@@ -0,0 +1,5 @@
+
+document.querySelector( '.menu-toggler' ).addEventListener( 'click', function() {
+        document.querySelector( '.main-menu-links' ).classList.toggle( 'hide-sm' );
+        });
+        
